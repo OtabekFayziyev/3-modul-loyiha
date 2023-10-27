3f70752c388ad9de75fb45b0436d4ca38623a2cc
Otabek <fayziyevotabek32@gmail.com>
2023-10-28 01:31:29 +0500

    3 loyiha start. change tabs contens write in js file

---
+++ js/script.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const tabheader__parent = document.querySelector('.tabheader__items'),
+        tabs = document.querySelectorAll('.tabheader__item'),
+        tabsContent = document.querySelectorAll('.tabcontent')
+
+    function hideHeaderItem() {
+        tabsContent.forEach((item) => {
+            item.classList.add('hide')
+            item.classList.remove('show', 'fade')
+
+        })
+
+        tabs.forEach((item) => {
+            item.classList.remove('tabheader__item_active')
+        })
+    }
+
+    function showHeaderItem(i = 0) {
+        tabsContent[i].classList.add('show', 'fade')
+        tabsContent[i].classList.remove('hide')
+        tabs[i].classList.add('tabheader__item_active')
+
+    }
+
+    hideHeaderItem()
+    showHeaderItem()
+
+    tabheader__parent.addEventListener('click', (event) => {
+        const target = event.target
+        if (target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, idx) => {
+                if (target == item) {
+                    hideHeaderItem()
+                    showHeaderItem(idx)
+                }
+            })
+        }
+    })
+
+})
